5cf31b460c00210dd35303c5f909bdd9e1ff88be
Joaquín <joaquin.suarezvallejos@gmail.com>
2025-03-15 12:54:07 -0300

    Alas del avion

---
+++ Ala.java
@@ -0,0 +1,39 @@
+package Partes;
+
+public class Ala {
+   private double envergadura;
+   private String color;
+   private String tipoMaterial;
+   private int flap = 0;
+
+    public Ala(double envergadura, String color, String tipoMaterial) {
+        this.envergadura = envergadura;
+        this.color = color;
+        this.tipoMaterial = tipoMaterial;
+    }
+
+    public int cambiarFlap (boolean subir){
+        if (subir){
+            this.flap = this.flap + 1;
+        }else{
+            this.flap = this.flap - 1;
+        }
+        return this.flap;
+    }
+
+    public int getFlap() {
+        return flap;
+    }
+
+
+    public double getEnvergadura() {
+        return envergadura;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getTipoMaterial() {
+        return tipoMaterial;
+    }
+}
+ 
