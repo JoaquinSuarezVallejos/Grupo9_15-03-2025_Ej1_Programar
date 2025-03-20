45dd75cf70164f50975b822b2e49ae6f5022f909
Joaquín <joaquin.suarezvallejos@gmail.com>
2025-03-20 19:46:51 -0300

    main update

--- Main.java
+++ Main.java
@@ -3,33 +3,25 @@ import Partes.Ala;
 import Partes.MotorDeAvion;
 import Vuelo.SistemaDeControlDeVuelo;
 
-import javax.xml.crypto.Data;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-    //ALAS DEL AVION
-    // Crear las instancias de las alas
-    Ala alaIzquierda = new Ala(10, "Blanco", "Metal");
-    Ala alaDerecha = new Ala(10, "Blanco", "Metal");
+        // ALAS DEL AVION
+        // Crear las instancias de las alas
+        Ala alaIzquierda = new Ala(10, "Blanco", "Metal");
+        Ala alaDerecha = new Ala(10, "Blanco", "Metal");
 
-    // Crear una lista de alas
-    Ala[] listAla = {alaIzquierda,alaDerecha};
+        // Crear una lista de alas
+        Ala[] listAla = { alaIzquierda, alaDerecha };
 
+        // MOTOR DEL AVION
+        MotorDeAvion motor = new MotorDeAvion("Boeing", 1000, 2500);
 
-    //MOTOR DEL AVION
-    MotorDeAvion motor = new MotorDeAvion("Boeing",1000, 2500);
+        // SISTEMA DE CONTROL DE VUELO DEL AVION
+        SistemaDeControlDeVuelo sisVuelo = new SistemaDeControlDeVuelo("Boeing", 1010, "Manual");
 
-    //SISTEMA DE CONTROL DE VUELO DEL AVION
-    SistemaDeControlDeVuelo  sisVuelo = new SistemaDeControlDeVuelo("Boeing",1010,"Manual");
-
-    //ARMADO DEL PRIMER AVION
-
-    Avion primerAvion = new Avion("Boeing","747",motor,sisVuelo,listAla,300,true);
+        // ARMADO DEL PRIMER AVION
+        Avion primerAvion = new Avion("Boeing", "747", motor, sisVuelo, listAla, 300, true);
 
         System.out.print(primerAvion.toString());
-
     }
-
-
 }
